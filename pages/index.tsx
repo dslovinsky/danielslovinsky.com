d6b5850f158476cf1eef3732ea67fbe05a894484
Daniel Slovinsky <slovinsky1993@gmail.com>
2021-06-27 22:43:56 -0700

    Updates all dependencies to latest versions

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,8 +7,15 @@ export default function Home() {
     <>
       <NextSeo title="Website coming soon... | Daniel Slovinsky" />
       <div className="flex flex-col justify-center items-center h-screen">
-        <h1 className={`${styles.placeholder} font-bold my-8 text-4xl`}>Website coming soon...</h1>
-        <img alt="Site under construction" src="construction.svg" width="512px" height="319px" />
+        <h1 className={`${styles.placeholder} font-bold my-8 text-4xl`}>
+          Website coming soon...
+        </h1>
+        <img
+          alt="Site under construction"
+          src="construction.svg"
+          width="512px"
+          height="319px"
+        />
       </div>
     </>
   );
