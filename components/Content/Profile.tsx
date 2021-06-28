02e270da017a7f9a7add9ff545960b20fd7f6573
Daniel Slovinsky <slovinsky1993@gmail.com>
2021-06-28 03:12:26 -0700

    Creates basic resume template

--- components/Content/Profile.tsx
+++ components/Content/Profile.tsx
@@ -1,11 +1,30 @@
+import Image from 'next/image';
+import me from 'public/photos/me.png';
 import styles from 'styles/index.module.scss';
 
 export default function Profile() {
   return (
-    <div className={styles.profile}>
-      <h1>Daniel Slovinsky</h1>
-      <h2>Frontend Web Developer at Verd.es</h2>
-      <h2>San Diego, CA</h2>
+    <div className={styles.profile_container}>
+      <div className={styles.profile}>
+        <div className={styles.photo_container}>
+          <Image
+            src={me}
+            alt="Daniel Slovinsky"
+            className={styles.photo}
+            height={200}
+            width={200}
+          />
+        </div>
+        <h1 className="text-2xl">Daniel Slovinsky</h1>
+        <h2>Front-end Web Developer at Verd.es</h2>
+        <h2>San Diego, CA</h2>
+        <p className="max-w-5xl p-4 text-center">
+          Driven, self-taught web developer focused on creating elegant,
+          efficient solutions that require minimal maintenance. Looking for
+          opportunities that allow me to build upon my experience with modern
+          web development tools and expand into new areas.
+        </p>
+      </div>
     </div>
   );
 }
--- components/Content/Resume.tsx
+++ components/Content/Resume.tsx
@@ -1,94 +1,103 @@
 import styles from 'styles/index.module.scss';
 
+type Job = {
+  title: string;
+  employer: string;
+  dates: string;
+  location: string;
+  duties: string[];
+};
+
+type School = {
+  name: string;
+  degree: string;
+  dates: string;
+};
+
+const jobs: Job[] = [
+  {
+    title: 'Front-end Web Developer',
+    employer: 'Verd.es',
+    dates: '2020-present',
+    location: 'San Diego, CA',
+    duties: [
+      'Develop and maintain a website that allows users in Spain to find nearby marijuana dispensaries',
+      'Optimized webpages to improve speed by over 90%',
+      'Work with a small team to share ideas and exchange code feedback',
+      'Use an array of tools including Next.js, React, TypeScript, JavaScript, and Tailwind',
+    ],
+  },
+  {
+    title: 'English Teacher',
+    employer: 'Buwon Middle School',
+    dates: '2018-2020',
+    location: 'Incheon, South Korea',
+    duties: [
+      'Taught 29 classes of 34-36 male students over 20 class hours per week schedule alongside multiple co-teachers',
+      'Taught additional after-school and weekend classes for parents, teachers, and students from San Gok Girls Middle School',
+      'Created custom lesson plans from scratch for each lesson taught',
+      'Assisted co-teachers and other Buwon Middle School faculty with their English education curriculum',
+    ],
+  },
+  {
+    title: 'SEO Analyst',
+    employer: 'Radd Interactive',
+    dates: '2016-2018',
+    location: 'San Diego, CA',
+    duties: [
+      'Analyzed online search patterns and evaluate competition in client markets to create an effective SEO strategy',
+      "Adapted clients' websites to capitalize on organic traffic and improve SERP rank",
+      'Wrote custom macros in VBA for company-wide usage',
+    ],
+  },
+  {
+    title: 'Quality Assurance/Social Media Analyst',
+    employer: 'Social Intelligence Corp.',
+    dates: '2014-2016',
+    location: 'Santa Barbara, CA',
+    duties: [
+      'Trained and assisted analysts with new report types and provided feedback in weekly meetings',
+      'Reviewed reports to maintain highest possible accuracy and ensure all information is up-to-date',
+      'Performed background checks on job applicants, investigated insurance claims, and carried out due diligence on high-profile individuals',
+      'Compiled reports for clients based on findings within a strict deadline',
+    ],
+  },
+];
+
+const schools: School[] = [
+  {
+    name: 'University of California, Santa Barbara',
+    degree: "Bachelor's Degree in Communication",
+    dates: '2011-2014',
+  },
+];
+
 export default function Resume() {
   return (
-    <div>
+    <div className={styles.resume}>
       <div className={styles.experience}>
-        <h1>Experience</h1>
-        <div className={styles.verdes}>
-          <h2>Front-end Web Developer</h2>
-          <h3>Verd.es</h3>
-          <h3>2020-present</h3>
-          <h3>San Diego, CA</h3>
-          <p>
-            Develop and maintain a website that allows users in Spain to find
-            nearby marijuana dispensaries
-          </p>
-          <p>Optimized webpages to improve speed by over 90%.</p>
-          <p>
-            Work with a small team to share ideas and exchange code feedback
-          </p>
-          <p>
-            Use an array of tools including Next.js, React, TypeScript,
-            JavaScript, and Tailwind
-          </p>
-        </div>
-        <div className={styles.buwon}>
-          <h2>English Teacher</h2>
-          <h3>Buwon Middle School</h3>
-          <h3>2018-2020</h3>
-          <h3>Incheon, South Korea</h3>
-          <p>
-            Taught 29 classes of 34-36 male students over 20 class hours per
-            week schedule alongside multiple co-teachers.
-          </p>
-          <p>
-            Taught additional after-school and weekend classes for parents,
-            teachers, and students from San Gok Girls Middle School.
-          </p>
-          <p>
-            Created custom lesson plans from scratch for each lesson taught.
-          </p>
-          <p>
-            Assisted co-teachers and other Buwon Middle School faculty with
-            their English education curriculum
-          </p>
-        </div>
-        <div className={styles.radd}>
-          <h2>English Teacher</h2>
-          <h3>SEO Analyst</h3>
-          <h3>2016-2018</h3>
-          <h3>San Diego, CA</h3>
-          <p>
-            Analyzed online search patterns and evaluate competition in client
-            markets to create an effective SEO strategy
-          </p>
-          <p>
-            Adapted clients' websites to capitalize on organic traffic and
-            improve SERP rank
-          </p>
-          <p>Wrote custom macros in VBA for company-wide usage</p>
-        </div>
-        <div className={styles.social_intel}>
-          <h2>Quality Assurance/Social Media Analyst</h2>
-          <h3>Social Intelligence Corp.</h3>
-          <h3>2014-2016</h3>
-          <h3>Santa Barbara, CA</h3>
-          <p>
-            Trained and assisted analysts with new report types and provided
-            feedback in weekly meetings
-          </p>
-          <p>
-            Reviewed reports to maintain highest possible accuracy and ensure
-            all information is up-to-date
-          </p>
-          <p>
-            Performed background checks on job applicants, investigated
-            insurance claims, and carried out due diligence on high-profile
-            individuals.
-          </p>
-          <p>
-            Compiled reports for clients based on findings within a strict
-            deadline.
-          </p>
-        </div>
+        <h1>— Experience —</h1>
+        {jobs.map(({title, employer, dates, location, duties}) => (
+          <div>
+            <h2>{title}</h2>
+            <h3>{employer}</h3>
+            <h3>{dates}</h3>
+            <h3>{location}</h3>
+            {duties.map((duty) => (
+              <p>- {duty}</p>
+            ))}
+          </div>
+        ))}
       </div>
       <div className={styles.education}>
-        <div>
-          <h2>University of California, Santa Barbara</h2>
-          <h3>Bachelor's Degree in Communication</h3>
-          <h3>2011-2014</h3>
-        </div>
+        <h1>— Education —</h1>
+        {schools.map(({name, degree, dates}) => (
+          <div>
+            <h2>{name}</h2>
+            <h3>{degree}</h3>
+            <h3>{dates}</h3>
+          </div>
+        ))}
       </div>
     </div>
   );
