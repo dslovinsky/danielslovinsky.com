10691812e9318afad29c110664674e0daa5b540e
Daniel Slovinsky <slovinsky1993@gmail.com>
2021-08-04 08:32:29 -0700

    Adds skill boxes and basic styling

--- components/Content/Skills.tsx
+++ components/Content/Skills.tsx
@@ -1,9 +1,38 @@
-import styles from 'styles/index.module.scss';
+import styles from 'styles/components/skills.module.scss';
+
+const skillSet = [
+  {
+    name: 'Javascript',
+  },
+  {
+    name: 'Typescript',
+  },
+  {
+    name: 'React',
+  },
+  {
+    name: 'Next.js',
+  },
+  {
+    name: 'CSS',
+  },
+  {
+    name: 'SASS',
+  },
+  {
+    name: 'Tailwind',
+  },
+];
 
 export default function Skills({...props}) {
   return (
-    <div {...props} className={styles.skills}>
-      Skills
+    <div {...props} className={`${styles.section} h-screen`}>
+      <h1 className="text-3xl my-8 font-bold self-center">— Skills —</h1>
+      <div className={styles.skills_container}>
+        {skillSet.map(({name}) => (
+          <div id={name} className={styles.skill}>{name}</div>
+        ))}
+      </div>
     </div>
   );
 }
