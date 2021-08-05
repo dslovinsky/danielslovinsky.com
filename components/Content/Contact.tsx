40546ff2d346c762ef3409fc3fe5922890466ebe
Daniel Slovinsky <slovinsky1993@gmail.com>
2021-08-05 02:37:56 -0700

    Adds initial form styling

--- components/Content/Contact.tsx
+++ components/Content/Contact.tsx
@@ -1,4 +1,5 @@
 import {TextField} from '@material-ui/core';
+import TextareaAutosize from '@material-ui/core/TextareaAutosize';
 
 import styles from 'styles/components/contact.module.scss';
 
@@ -7,10 +8,19 @@ export default function Contact({...props}) {
     <div {...props} className={`${styles.section} h-screen`}>
       <h1 className="text-3xl my-8 font-bold self-center">— Contact Me —</h1>
       <form className={styles.form}>
-        <TextField placeholder="Name" />
-        <TextField placeholder="Email" />
-        <TextField placeholder="Subject" />
-        <TextField placeholder="Message" />
+        <div className={styles.toprow}>
+          <TextField placeholder="Name" />
+          <TextField placeholder="Email" />
+        </div>
+        <div className={styles.subject}>
+          <TextField placeholder="Subject" />
+        </div>
+        <div className={styles.message}>
+          <TextareaAutosize
+            placeholder="Message"
+            minRows={6}
+          />
+        </div>
       </form>
     </div>
   );
