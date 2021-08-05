164fd31496a7645c65024fbdda2dff9696946169
Daniel Slovinsky <slovinsky1993@gmail.com>
2021-08-04 19:28:28 -0700

    Adds Skill logos

--- fontawesome/freeBrands.ts
+++ fontawesome/freeBrands.ts
@@ -1,8 +1,1 @@
-export {
-  faCss3,
-  faGithub,
-  faJsSquare,
-  faLinkedinIn,
-  faReact,
-  faSass,
-} from '@fortawesome/free-brands-svg-icons';
+export {faGithub, faLinkedinIn} from '@fortawesome/free-brands-svg-icons';
