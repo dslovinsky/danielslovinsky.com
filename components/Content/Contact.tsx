f1d29119969dc4bfb4e75693f93880484405408e
Daniel Slovinsky <slovinsky1993@gmail.com>
2021-08-06 18:55:00 -0700

    Adds paragraph before form and fixes placeholder text styles

--- components/Content/Contact.tsx
+++ components/Content/Contact.tsx
@@ -8,6 +8,11 @@ export default function Contact({...props}) {
     <div {...props} className={`${styles.section} h-screen`}>
       <h1 className="text-3xl my-8 font-bold self-center">— Contact Me —</h1>
       <form className={styles.form}>
+      <p>
+        Shoot me a message any time with comments, questions, offers, or just to
+        say hi. While I'm primarily interested in full-time employment, I'm open
+        to opportunities of any kind.
+      </p>
         <div className={styles.toprow}>
           <TextField placeholder="Name" />
           <TextField placeholder="Email" />
@@ -16,10 +21,7 @@ export default function Contact({...props}) {
           <TextField placeholder="Subject" />
         </div>
         <div className={styles.message}>
-          <TextareaAutosize
-            placeholder="Message"
-            minRows={6}
-          />
+          <TextareaAutosize placeholder="Message" minRows={6} />
         </div>
       </form>
     </div>
