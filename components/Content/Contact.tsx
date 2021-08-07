e0ad65d7a2a1265aab2922e08f0515e64872fcfc
Daniel Slovinsky <slovinsky1993@gmail.com>
2021-08-06 20:59:40 -0700

    Adds basic submit button to form

--- components/Content/Contact.tsx
+++ components/Content/Contact.tsx
@@ -1,13 +1,18 @@
-import {TextField} from '@material-ui/core';
-import TextareaAutosize from '@material-ui/core/TextareaAutosize';
+import {TextField, TextareaAutosize, Button} from '@material-ui/core';
+import {FormEvent} from 'react';
 
 import styles from 'styles/components/contact.module.scss';
+
+const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+  e.preventDefault()
+  console.log('sent')
+}
 
 export default function Contact({...props}) {
   return (
     <div {...props} className={`${styles.section} h-screen`}>
       <h1 className="text-3xl my-8 font-bold self-center">— Contact Me —</h1>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={onSubmit}>
       <p>
         Shoot me a message any time with comments, questions, offers, or just to
         say hi. While I'm primarily interested in full-time employment, I'm open
@@ -23,6 +28,7 @@ export default function Contact({...props}) {
         <div className={styles.message}>
           <TextareaAutosize placeholder="Message" minRows={6} />
         </div>
+        <Button className={styles.send} type="submit">Send</Button>
       </form>
     </div>
   );
