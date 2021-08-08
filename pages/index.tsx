98a42741e9813e3c187e24824305518f681b8364
Daniel Slovinsky <slovinsky1993@gmail.com>
2021-08-07 20:21:33 -0700

    Readds animation to home screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import {NextSeo} from 'next-seo';
 
-// import GoL from '@components/Animations/GoL';
+import GoL from '@components/Animations/GoL';
 import About from '@components/Content/About';
 import Title from '@components/Content/Title';
 import Skills from '@components/Content/Skills';
@@ -9,16 +9,16 @@ import Portfolio from '@components/Content/Portfolio';
 import NavBar from '@components/Navs/NavBar';
 import ScrollAnchor from '@components/Navs/ScrollAnchor';
 import BackToTop from '@components/Navs/BackToTop';
-// import useWindowDimensions from '@utils/useWindowDimensions';
+import useWindowDimensions from '@utils/useWindowDimensions';
 
 import styles from 'styles/index.module.scss';
 
 export default function Home() {
-  // const {width, height} = useWindowDimensions();
+  const {width, height} = useWindowDimensions();
 
   // calculate dimensions of canvas to determine if it needs to rerender on window size change
-  // const calcW = Math.ceil(width / 100) * 10;
-  // const calcH = Math.ceil(height / 100) * 10;
+  const calcW = Math.ceil(width / 100) * 10;
+  const calcH = Math.ceil(height / 100) * 10;
 
   return (
     <>
@@ -27,7 +27,7 @@ export default function Home() {
         <NavBar />
         <ScrollAnchor />
         <Title />
-        {/* {width && (
+        {width && (
           <GoL
             key={`${calcW}${calcH}`}
             width={calcW}
@@ -36,11 +36,10 @@ export default function Home() {
             streak={0.5}
             id="Home"
           />
-        )} */}
-        <div id="Home" className="h-screen">
+        )}
+        {/* <div id="Home" className="h-screen">
           GoL placeholder
-        </div>
-        {/* <SectionPlaceholder /> */}
+        </div> */}
         <About id="About" />
         <Portfolio id="Portfolio" />
         <Skills id="Skills" />
