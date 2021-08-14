5fbc07d206dbdd4a28a63fd25350442972003b61
Daniel Slovinsky <slovinsky1993@gmail.com>
2021-08-13 23:00:09 -0700

    Fixes interface/Email being called with nonexistent default export

--- pages/api/email.ts
+++ pages/api/email.ts
@@ -1,7 +1,7 @@
 import {NextApiRequest, NextApiResponse} from 'next';
 import sgMail from '@sendgrid/mail';
 
-import EmailType from '@interfaces/email';
+import {Email as EmailType} from '@interfaces/email';
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
