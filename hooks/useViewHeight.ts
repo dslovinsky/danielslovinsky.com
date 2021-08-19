f12e3e45e5b1e6ecfb8ab428eb6c5db79e769cc3
Daniel Slovinsky <slovinsky1993@gmail.com>
2021-08-19 00:50:18 -0700

    Separate vh solution by fixing vh calc on title and GoL to use calc

--- hooks/useViewHeight.ts
+++ hooks/useViewHeight.ts
@@ -1,12 +1,12 @@
-// import useWindowDimensions from './useWindowDimensions';
+import useWindowDimensions from './useWindowDimensions';
 import {useEffect} from 'react';
 
 // @see https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
 export default function useViewHeight() {
-  // const {height} = useWindowDimensions();
+  const {height} = useWindowDimensions();
 
   useEffect(() => {
     const vh = window.innerHeight / 100;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
-  }, []);
+  }, [height]);
 }
