eb37c394029046830881a15f4ab1801c5f10d075
Daniel Slovinsky <65476034+dslovinsky@users.noreply.github.com>
2022-08-21 14:43:17 -0700

    Requires more than one word in contact message and moves font imports to _document (#49)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,10 @@
-import Document, {DocumentContext} from 'next/document';
+import Document, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document';
 import {ServerStyleSheet} from 'styled-components';
 
 export default class MyDocument extends Document {
@@ -26,5 +32,42 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            key="favicon"
+            rel="icon"
+            type="image/ico"
+            href="/icons/favicon.ico"
+          />
+          <link
+            key="preconnect-google-fonts"
+            rel="preconnect"
+            href="https://fonts.googleapis.com"
+            crossOrigin="anonymous"
+          />
+          <link
+            key="preconnect-fonts-gstatic"
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
+          <link
+            key="google-fonts"
+            href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400;1,700&family=Roboto:wght@300;400;500;700&display=swap"
+            rel="stylesheet"
+          />
+          <noscript>You need to enable Javascript to use this app.</noscript>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
