03b1041927ebecde0ea469dc6e7b196a7d2da4f1
Daniel Slovinsky <slovinsky1993@gmail.com>
2023-03-25 15:40:11 -0700

    Adds Contentful env variables

--- environment.d.ts
+++ environment.d.ts
@@ -3,5 +3,10 @@ declare namespace NodeJS {
     NODE_ENV: 'development' | 'production' | 'test';
     PERSONAL_EMAIL: string;
     SENDGRID_API_KEY: string;
+    CONTENTFUL_SPACE: string;
+    CONTENTFUL_DELIVERY_KEY: string;
+    CONTENTFUL_PREVIEW_KEY: string;
+    CONTENTFUL_ENV: string;
+    CONTENTFUL_HOST: string;
   }
 }
