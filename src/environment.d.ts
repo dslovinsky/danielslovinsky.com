82f7cee7b45e10d039be7cb01e251c5bcec592a8
Daniel Slovinsky <slovinsky1993@gmail.com>
2023-03-25 18:50:36 -0700

    Creates initial page queries from Contentful

--- src/environment.d.ts
+++ src/environment.d.ts
@@ -3,9 +3,9 @@ declare namespace NodeJS {
     NODE_ENV: 'development' | 'production' | 'test';
     PERSONAL_EMAIL: string;
     SENDGRID_API_KEY: string;
-    CONTENTFUL_SPACE: string;
-    CONTENTFUL_DELIVERY_KEY: string;
-    CONTENTFUL_PREVIEW_KEY: string;
+    CONTENTFUL_SPACE_ID: string;
+    CONTENTFUL_DELIVERY_TOKEN: string;
+    CONTENTFUL_PREVIEW_TOKEN: string;
     CONTENTFUL_ENV: string;
     CONTENTFUL_HOST: string;
   }
---
+++ src/pages/[[...slug]].tsx
@@ -0,0 +1,64 @@
+import contentfulClient from 'contentful/client';
+
+import type { Entry } from 'contentful';
+import type { ITemplatePage, ITemplatePageFields } from 'contentful/types';
+import type { GetStaticPaths, GetStaticProps, NextPage } from 'next';
+
+interface PageProps {
+  pageProps: ITemplatePage;
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const { items: allPageSlugs } = await contentfulClient.getEntries<ITemplatePageFields>({
+    content_type: 'templatePage',
+    // Contentful only allows up to 1000 entries to be queried at once
+    // see @https://www.contentful.com/developers/docs/javascript/tutorials/using-js-cda-sdk/
+    limit: 1000,
+    select: ['fields.slug'],
+  });
+
+  const paths = allPageSlugs.map(page => {
+    const slug = page.fields.slug;
+
+    return {
+      params: {
+        // since this file uses catch-all routes, returning an empty array generates the homepage
+        // see @https://nextjs.org/docs/routing/dynamic-routes#optional-catch-all-routes
+        slug: slug === '/' ? [] : slug.split('/'),
+      },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+interface StaticProps {
+  pageProps: Entry<ITemplatePageFields>;
+}
+
+export const getStaticProps: GetStaticProps<StaticProps> = async ({ params }) => {
+  const pageSlug = (params?.slug as string[])?.join('/') || '/';
+
+  const { items } = await contentfulClient.getEntries<ITemplatePageFields>({
+    content_type: 'templatePage',
+    limit: 1,
+    // Contentful only allows up to a depth of 10 nested entries
+    // see @https://www.contentful.com/developers/docs/references/content-delivery-api/#/reference/links/retrieval-of-linked-items
+    include: 10,
+    'fields.slug': pageSlug,
+  });
+
+  return {
+    props: {
+      pageProps: items[0],
+    },
+    revalidate: 600,
+  };
+};
+
+const TemplatePage: NextPage<PageProps> = ({ pageProps }) => <h1>{pageProps?.fields?.internalName || 'home'}</h1>;
+
+export default TemplatePage;
