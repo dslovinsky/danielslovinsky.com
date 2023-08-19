05255e0d5b788d3a83bf5bb49d5dac12c0d7b0df
Daniel Slovinsky <slovinsky1993@gmail.com>
2023-08-19 03:13:13 -0700

    refactor: remove metadataBase since it's set by Vercel by default

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { name } from 'utils/constants';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  metadataBase: new URL(`https://${process.env.VERCEL_URL}`),
   title: {
     template: `%s | ${name}`,
     default: name,
