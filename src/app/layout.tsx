557c69d0e890721f8ec66940fe9d928534611d70
Daniel Slovinsky <slovinsky1993@gmail.com>
2023-08-19 16:11:59 -0700

    feat: install and configure tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import 'styles/global.css';
 import { name } from 'utils/constants';
 
 import type { Metadata } from 'next';
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    extend: {
+      spacing: {
+        18: '4.5rem',
+      },
+    },
+  },
+  plugins: [],
+};
