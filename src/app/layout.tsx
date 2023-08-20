a66d99a8847d1b30e7917758cf5c9d2b4491abd3
Daniel Slovinsky <slovinsky1993@gmail.com>
2023-08-19 18:08:30 -0700

    feat: adds fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,23 @@
 import 'styles/global.css';
+import { Inter, Titillium_Web } from 'next/font/google';
+
 import { name } from 'utils/constants';
 
 import type { Metadata } from 'next';
 import type { ReactNode } from 'react';
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+});
+
+const titilliumWeb = Titillium_Web({
+  weight: ['400', '600', '700', '900'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-titillium',
+});
 
 export const metadata: Metadata = {
   title: {
@@ -12,7 +27,7 @@ export const metadata: Metadata = {
 };
 
 const RootLayout = ({ children }: { children: ReactNode }) => (
-  <html lang="en">
+  <html lang="en" className={`${inter.variable} ${titilliumWeb.variable}`}>
     <body>{children}</body>
   </html>
 );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
+    fontFamily: {
+      inter: ['var(--font-inter)'],
+      titillium: ['var(--font-titillium)'],
+    },
     extend: {
       spacing: {
         18: '4.5rem',
