4c63451812fe5e6b6d8fa829cb506323c37d2d62
Daniel Slovinsky <slovinsky1993@gmail.com>
2023-08-22 04:07:21 -0700

    feat: create header mobile menu

--- src/components/Header/Hamburger.tsx
+++ src/components/Header/Hamburger.tsx
@@ -1,11 +1,23 @@
 'use client';
-import { type ComponentPropsWithoutRef, type FC, useState } from 'react';
+import { type ComponentPropsWithoutRef, type FC, type ReactNode, useState } from 'react';
 import { twMerge } from 'tailwind-merge';
 
-type HamburgerProps = ComponentPropsWithoutRef<'button'>;
+interface HamburgerProps extends ComponentPropsWithoutRef<'button'> {
+  children: ReactNode;
+}
 
-const Hamburger: FC<HamburgerProps> = ({ className, ...props }) => {
-  const [isActive, setIsActive] = useState(false);
+const Hamburger: FC<HamburgerProps> = ({ className, children, ...props }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onClick = () => {
+    if (isOpen) {
+      document.body.style.overflow = 'auto';
+    } else {
+      document.body.style.overflow = 'hidden';
+    }
+
+    setIsOpen(!isOpen);
+  };
 
   const lines = [
     'origin-top-left rotate-45 translate-x-1',
@@ -16,15 +28,24 @@ const Hamburger: FC<HamburgerProps> = ({ className, ...props }) => {
   return (
     <button
       className={twMerge(`flex h-6 w-6 flex-col justify-between py-[2px] ${className}`)}
-      onClick={() => setIsActive(!isActive)}
+      onClick={onClick}
       {...props}
     >
       {Array.from({ length: 3 }, (_, index) => (
         <div
           key={index}
-          className={`h-[3px] w-full rounded-xl bg-white transition-all duration-200 ${isActive && lines[index]}`}
+          className={`h-[3px] w-full rounded-xl bg-white transition-all duration-200 ${isOpen ? lines[index] : ''}`}
         />
       ))}
+      {
+        <div
+          className={`absolute left-0 top-[calc(100%+2px)] z-50 h-[calc(var(--dvh,1vh)*100-56px)] w-full overflow-hidden transition-[max-height] duration-200 ${
+            isOpen ? 'max-h-screen' : 'max-h-0'
+          }`}
+        >
+          {children}
+        </div>
+      }
     </button>
   );
 };
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,35 +3,39 @@ import Link from 'next/link';
 import graphqlQuery from 'graphql/apolloClient';
 import { GlobalHeaderDocument } from 'graphql/sdk';
 
-import Button from 'molecules/Button';
 import Icon from 'molecules/Icon';
 
 import Hamburger from 'components/Header/Hamburger';
+import MenuItem from 'components/Header/MenuItem';
+import MobileMenu from 'components/Header/MobileMenu';
 
 import type { FC } from 'react';
 
 const Header: FC = async () => {
   const { componentHeader } = await graphqlQuery<GlobalHeaderQuery>(GlobalHeaderDocument);
 
+  if (!componentHeader) {
+    throw new Error('Component Header data is missing');
+  }
+
+  const { links, mobileBottomLinks } = componentHeader;
+
   return (
-    <nav className="flex justify-center border border-b-2 border-maya-blue-10 py-4">
+    <nav className="relative flex justify-center border-b-2 border-b-maya-blue-10 py-4">
       <div className="flex w-full max-w-column justify-between px-4 md:px-14 xl:px-24">
         <Link href="/">
           <Icon icon="ds-logo" size={24} className="md:h-8 md:w-8" />
         </Link>
-        {componentHeader?.links && (
-          <div className="hidden gap-y-10 sm:flex">
-            {componentHeader.links.map(({ id, ...rest }) => (
-              <Button
-                key={id}
-                id={id}
-                {...rest}
-                className="relative flex items-center pb-1 font-titillium text-md font-semibold uppercase tracking-lg after:absolute after:bottom-0 after:left-1/2 after:h-0.5 after:w-0 after:-translate-x-1/2 after:bg-maya-blue after:transition-[width] after:duration-100 after:ease-in-out hover:after:w-full"
-              />
+        {links && (
+          <div className="hidden gap-x-10 sm:flex">
+            {links.map(link => (
+              <MenuItem key={link.id} {...link} />
             ))}
           </div>
         )}
-        <Hamburger className="flex sm:hidden" />
+        <Hamburger className="flex sm:hidden">
+          <MobileMenu links={links} mobileBottomLinks={mobileBottomLinks} />
+        </Hamburger>
       </div>
     </nav>
   );
