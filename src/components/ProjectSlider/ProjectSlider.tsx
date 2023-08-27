e9106bc4c427a1a70d700c5e0eb83483c8f62dc8
Daniel Slovinsky <slovinsky1993@gmail.com>
2023-08-27 04:47:05 -0700

    fix: corrects some skill image positioning

--- src/components/ProjectSlider/ProjectSlider.tsx
+++ src/components/ProjectSlider/ProjectSlider.tsx
@@ -1,5 +1,9 @@
+import SectionHeading from 'molecules/SectionHeading';
+
 import type { FC } from 'react';
 
-const ProjectSlider: FC<ComponentProjectSliderFragment> = ({ heading }) => <div>{heading}</div>;
+const ProjectSlider: FC<ComponentProjectSliderFragment> = ({ heading }) => (
+  <div>{heading && <SectionHeading heading={heading} alignOnDesktop="left" />}</div>
+);
 
 export default ProjectSlider;
