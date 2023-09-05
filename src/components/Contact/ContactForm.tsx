d61c19390a3fcdd650bc99d6216a35ba3298287a
Daniel Slovinsky <slovinsky1993@gmail.com>
2023-09-05 01:58:35 -0700

    wip: add basic styling to form inputs

--- src/components/Contact/ContactForm.tsx
+++ src/components/Contact/ContactForm.tsx
@@ -16,12 +16,12 @@ const inputClasses =
   'border-white-25 border-2 border-solid bg-transparent p-4 focus-within:border-white focus-visible:outline-none';
 
 const ContactForm: FC = () => (
-  <form onSubmit={onSubmit} className="grid grid-cols-2 gap-4">
+  <form onSubmit={onSubmit} className="grid w-full grid-cols-2 gap-4">
     <input type="text" name="name" placeholder="Name" className={inputClasses} />
     <input type="email" name="email" placeholder="Email" className={inputClasses} />
     <input type="text" name="subject" placeholder="Subject" className={`col-span-2 ${inputClasses}`} />
-    <textarea name="message" placeholder="Message" className={`col-span-2 ${inputClasses}`} />
-    <button type="submit" className="col-span-2 uppercase">
+    <textarea name="message" placeholder="Message" className={`col-span-2 min-h-[180px] ${inputClasses}`} />
+    <button type="submit" className="col-span-2 border-2 border-solid border-maya-blue px-10 py-4 uppercase">
       Send Message
     </button>
   </form>
