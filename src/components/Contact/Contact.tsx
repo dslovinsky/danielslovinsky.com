15bd4bbd81ff49062e89f34aeed6b17ce5f03d9b
Daniel Slovinsky <slovinsky1993@gmail.com>
2023-09-10 03:23:37 -0700

    feat: add game of life to contact

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,15 +1,19 @@
 import SectionHeading from 'molecules/SectionHeading';
 import StructuredText from 'molecules/StructuredText';
 
+import ContactAnimation from 'components/Contact/ContactAnimation';
 import ContactForm from 'components/Contact/ContactForm';
 
 import type { FC } from 'react';
 
 const Contact: FC<ComponentContactFragment> = ({ heading, body }) => (
-  <div className="mx-auto flex max-w-3xl flex-col items-center">
-    {heading && <SectionHeading heading={heading} alignOnDesktop="center" />}
-    {body && <StructuredText data={body} className="mb-6 mt-10 flex flex-col items-center gap-y-10 text-center" />}
-    <ContactForm />
+  <div className="mt-24">
+    <ContactAnimation />
+    <div className="mx-auto flex max-w-3xl flex-col items-center">
+      {heading && <SectionHeading heading={heading} alignOnDesktop="center" />}
+      {body && <StructuredText data={body} className="mb-6 mt-10 flex flex-col items-center gap-y-10 text-center" />}
+      <ContactForm />
+    </div>
   </div>
 );
 
